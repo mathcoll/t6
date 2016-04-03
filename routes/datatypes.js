99e7302ef447fe3b164863a0501a31576632f935
mathcoll <mathieu@internetcollaboratif.info>
2016-04-03 20:49:46 +0200

    Set Admin permissions to Units and DataTypes

--- routes/datatypes.js
+++ routes/datatypes.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var DataTypeSerializer = require('../serializers/datatype');
 var datatypes;
 var users;
+var tokens;
 
 router.get('/', function (req, res) {
 	datatypes	= db.getCollection('datatypes');
@@ -11,55 +12,64 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:datatype_id([0-9a-z\-]+)', function (req, res) {
-	var datatype_id = (req.params.datatype_id).toString(); //TODO: not always an Integer !!!
-	datatypes	= db.getCollection('datatypes');
-	//console.log(datatype_id);
-	res.send(new DataTypeSerializer(datatypes.find({ 'id': datatype_id })).serialize());
-});
-
-router.post('/', bearerAuth, function (req, res) {
-	// only for admins
-	datatypes	= db.getCollection('datatypes');
-	//console.log(datatypes);
-	var new_datatype = {
-		id:			uuid.v4(),
-		name:	req.body.name!==undefined?req.body.name:'unamed',
-	};
-	datatypes.insert(new_datatype);
-	//console.log(datatypes);
-	res.send({ 'code': 201, message: 'Created', datatype: new DataTypeSerializer(new_datatype).serialize() }, 201);
-});
-
-router.put('/:datatype_id([0-9a-z\-]+)', bearerAuth, function (req, res) {
-	// TODO: Impement permissions
 	var datatype_id = req.params.datatype_id;
 	datatypes	= db.getCollection('datatypes');
-	//console.log(datatypes);
-	var result;
-	datatypes.findAndUpdate(
-		function(i){return i.id==datatype_id},
-		function(item){
-			item.name		= req.body.name!==undefined?req.body.name:item.name;
-			result = item;
-		}
-	);
-	db.save();
-	res.send({ 'code': 200, message: 'Successfully updated', datatype: new DataTypeSerializer(result).serialize() }, 200);
+	var json = datatypes.find({ 'id': { '$eq': datatype_id } });
+	//console.log(json);
+	if ( json.length > 0 ) {
+		res.send(new DataTypeSerializer(json).serialize(), 200);
+	} else {
+		res.send({ 'code': 404, 'error': 'Not Found' }, 404);
+	}
 });
 
-router.delete('/:datatype_id([0-9a-z\-]+)', bearerAuth, function (req, res) {
-	// only for admins
-	//TODO: look to fail in deletion
-	var datatype_id = req.params.datatype_id; //TODO: not always an Integer !!!
-	datatypes	= db.getCollection('datatypes');
-	var d = datatypes.find({'id': { '$eq': datatype_id }});
-	//console.log(d);
-	if (d) {
-		datatypes.remove(d);
-		db.save();
-		res.send({ 'code': 200, message: 'Successfully deleted', removed_id: datatype_id }, 200); // TODO: missing serializer
+router.post('/', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		datatypes	= db.getCollection('datatypes');
+		var new_datatype = {
+			id:			uuid.v4(),
+			name:	req.body.name!==undefined?req.body.name:'unamed',
+		};
+		datatypes.insert(new_datatype);
+		res.send({ 'code': 201, message: 'Created', datatype: new DataTypeSerializer(new_datatype).serialize() }, 201);
 	} else {
-		res.send({ 'code': 404, message: 'Not Found' }, 404);
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
+	}
+});
+
+router.put('/:datatype_id([0-9a-z\-]+)', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		var datatype_id = req.params.datatype_id;
+		datatypes	= db.getCollection('datatypes');
+		var result;
+		datatypes.findAndUpdate(
+			function(i){return i.id==datatype_id},
+			function(item){
+				item.name		= req.body.name!==undefined?req.body.name:item.name;
+				result = item;
+			}
+		);
+		db.save();
+		res.send({ 'code': 200, message: 'Successfully updated', datatype: new DataTypeSerializer(result).serialize() }, 200);
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
+	}
+});
+
+router.delete('/:datatype_id([0-9a-z\-]+)', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		var datatype_id = req.params.datatype_id;
+		datatypes	= db.getCollection('datatypes');
+		var d = datatypes.find({'id': { '$eq': datatype_id }});
+		if (d) {
+			datatypes.remove(d);
+			db.save();
+			res.send({ 'code': 200, message: 'Successfully deleted', removed_id: datatype_id }, 200); // TODO: missing serializer
+		} else {
+			res.send({ 'code': 404, message: 'Not Found' }, 404);
+		}
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
 	}
 });
 
@@ -83,6 +93,35 @@ function bearerAuth(req, res, next) {
 		next();
 	} else {
 		res.send({ 'code': 403, 'error': 'Forbidden' }, 403);
+	}
+}
+
+function bearerAdmin(req, res, next) {
+	var bearerToken;
+	var bearerHeader = req.headers['authorization'];
+	tokens	= db.getCollection('tokens');
+	users	= db.getCollection('users');
+	if ( typeof bearerHeader !== 'undefined' ) {
+		var bearer = bearerHeader.split(" ");// TODO split with Bearer as prefix!
+		bearerToken = bearer[1];
+		req.token = bearerToken;
+		req.bearer = tokens.findOne(
+			{ '$and': [
+	           {'token': { '$eq': req.token }},
+	           {'expiration': { '$gte': moment().format('x') }},
+			]}
+		);
+		if ( !req.bearer ) {
+			res.send({ 'code': 403, 'error': 'Forbidden' }, 403);
+		} else {
+			if ( req.user = users.findOne({'id': { '$eq': req.bearer.user_id }, 'role': 'admin'}) ) {
+				next();
+			} else {
+				res.send({ 'code': 404, 'error': 'Not Found' }, 404);
+			}
+		}
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
 	}
 }
 
--- routes/units.js
+++ routes/units.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var UnitSerializer = require('../serializers/unit');
 var units;
 var users;
+var tokens;
 
 router.get('/', function (req, res) {
 	var type = req.query.type;
-	units	= db.getCollection('units');
-	//console.log( units );
+	units = db.getCollection('units');
 	if ( type === undefined ) {
 		var json = new UnitSerializer(units.find()).serialize();
 		res.send(json);
@@ -19,82 +19,101 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:unit_id([0-9a-z\-]+)', function (req, res) {
-	var unit_id = parseInt(req.params.unit_id); //TODO: not always an Integer !!!
-	units	= db.getCollection('units');
-	//console.log(unit_id);
-	var json = new UnitSerializer(units.find({'id': { '$eq': unit_id }})).serialize();
-	res.send(json);
-});
-
-router.post('/', bearerAuth, function (req, res) {
-	// only for admins
-	units	= db.getCollection('units');
-	//console.log(units);
-	var new_unit = {
-		id: uuid.v4(),
-		name:	req.body.name!==undefined?req.body.name:item.name,
-		format:	req.body.format!==undefined?req.body.format:item.format,
-		type: 	req.body.type!==undefined?req.body.type:item.type,
-	};
-	units.insert(new_unit);
-	//console.log(units);
-	res.send({ 'code': 201, message: 'Created', unit: new UnitSerializer(new_unit).serialize() }, 201);
-});
-
-router.put('/:unit_id([0-9a-z\-]+)', bearerAuth, function (req, res) {
-	// only for admins
 	var unit_id = req.params.unit_id;
 	units	= db.getCollection('units');
-	//console.log(units);
-	var result;
-	units.findAndUpdate(
-		function(i){return i.id==unit_id},
-		function(item){
-			item.name		= req.body.name!==undefined?req.body.name:item.name;
-			item.format		= req.body.format!==undefined?req.body.format:item.format;
-			item.type		= req.body.type!==undefined?req.body.type:item.type;
-			result = item;
-		}
-	);
-	db.save();
-	res.send({ 'code': 200, message: 'Successfully updated', unit: new UnitSerializer(result).serialize() }, 200);
-});
-
-router.delete('/:unit_id([0-9a-z\-]+)', bearerAuth, function (req, res) {
-	// TODO: Implement permissions
-	var unit_id = req.params.unit_id; //TODO: not always an Integer !!!
-	units	= db.getCollection('units');
-	var u = units.find({'id': { '$eq': unit_id }});
-	//console.log(u);
-	if (u) {
-		units.remove(u);
-		db.save();
-		res.send({ 'code': 200, message: 'Successfully deleted', removed_id: unit_id }, 200); // TODO: missing serializer
+	var json = units.find({ 'id': { '$eq': unit_id } });
+	console.log(json);
+	if ( json.length > 0 ) {
+		res.send(new UnitSerializer(json).serialize(), 200);
 	} else {
-		res.send({ 'code': 404, message: 'Not Found' }, 404);
+		res.send({ 'code': 404, 'error': 'Not Found' }, 404);
 	}
 });
 
-function bearerAuth(req, res, next) {
+router.post('/', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		units	= db.getCollection('units');
+		//console.log(units);
+		var new_unit = {
+			id: uuid.v4(),
+			name:	req.body.name!==undefined?req.body.name:'unamed',
+			format:	req.body.format!==undefined?req.body.format:'',
+			type:	req.body.type!==undefined?req.body.type:'',
+		};
+		units.insert(new_unit);
+		//console.log(units);
+		res.send({ 'code': 201, message: 'Created', unit: new UnitSerializer(new_unit).serialize() }, 201);
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
+	}
+});
+
+router.put('/:unit_id([0-9a-z\-]+)', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		var unit_id = req.params.unit_id;
+		units	= db.getCollection('units');
+		//console.log(units);
+		var result;
+		units.findAndUpdate(
+			function(i){return i.id==unit_id},
+			function(item){
+				item.name		= req.body.name!==undefined?req.body.name:item.name;
+				item.format		= req.body.format!==undefined?req.body.format:item.format;
+				item.type		= req.body.type!==undefined?req.body.type:item.type;
+				result = item;
+			}
+		);
+		db.save();
+		res.send({ 'code': 200, message: 'Successfully updated', unit: new UnitSerializer(result).serialize() }, 200);
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
+	}
+});
+
+router.delete('/:unit_id([0-9a-z\-]+)', bearerAdmin, function (req, res) {
+	if ( req.token ) {
+		var unit_id = req.params.unit_id;
+		units	= db.getCollection('units');
+		var u = units.find({'id': { '$eq': unit_id }});
+		//console.log(u);
+		if (u) {
+			units.remove(u);
+			db.save();
+			res.send({ 'code': 200, message: 'Successfully deleted', removed_id: unit_id }, 200); // TODO: missing serializer
+		} else {
+			res.send({ 'code': 404, message: 'Not Found' }, 404);
+		}
+	} else {
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
+	}
+});
+
+function bearerAdmin(req, res, next) {
 	var bearerToken;
 	var bearerHeader = req.headers['authorization'];
+	tokens	= db.getCollection('tokens');
 	users	= db.getCollection('users');
 	if ( typeof bearerHeader !== 'undefined' ) {
-		var bearer = bearerHeader.split(" ");
+		var bearer = bearerHeader.split(" ");// TODO split with Bearer as prefix!
 		bearerToken = bearer[1];
 		req.token = bearerToken;
-		var queryAdmin = {
-			'$and': [
-				{'role': 'admin'},
-				{'token': { '$eq': req.token }},
-			]
-		};
-		if (req.user = (users.find(queryAdmin))[0] == undefined ) {
+		req.bearer = tokens.findOne(
+			{ '$and': [
+	           {'token': { '$eq': req.token }},
+	           {'expiration': { '$gte': moment().format('x') }},
+			]}
+		);
+		if ( !req.bearer ) {
 			res.send({ 'code': 403, 'error': 'Forbidden' }, 403);
-		};
-		next();
+		} else {
+			if ( req.user = users.findOne({'id': { '$eq': req.bearer.user_id }, 'role': 'admin'}) ) {
+				next();
+			} else {
+				res.send({ 'code': 404, 'error': 'Not Found' }, 404);
+			}
+		}
 	} else {
-		res.send({ 'code': 403, 'error': 'Forbidden' }, 403);
+		res.send({ 'code': 401, 'error': 'Unauthorized' }, 401);
 	}
 }
 
