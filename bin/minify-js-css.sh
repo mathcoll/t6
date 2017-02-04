0e5953d3c967a3484bfb3216e73f3ef95ca902ac
mathcoll <mathieu@internetcollaboratif.info>
2017-02-04 20:31:29 +0100

    remove warning

--- bin/minify-js-css.sh
+++ bin/minify-js-css.sh
@@ -4,13 +4,13 @@ uglifyjs \
 	../public/js/bootstrap.js \
 	../public/js/t6.js \
 	-o ../public/js/t6.min.js \
-	-p 5 -c -m
+	-p 5 -m -c warnings=false
 echo Javascript minify: Completed
 
 uglifycss \
 	../public/css/bootstrap.min.css \
-	../public/css/t6.css \
 	../public/css/font-awesome/font-awesome.min-4.7.0.css \
+	../public/css/t6.css \
 	> ../public/css/t6.min.css
 	
 echo Stylesheet minify: Completed
