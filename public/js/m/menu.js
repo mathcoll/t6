1c41b0c96f5b2ae1681cc921dbff9e59fde83dc5
mathcoll <mathieu@internetcollaboratif.info>
2017-06-16 15:55:58 +0200

    Update FAB menu

--- public/js/m/menu.js
+++ public/js/m/menu.js
@@ -13,8 +13,8 @@
 	var login_button = document.querySelector('#login_button');
 	var profile_button = document.querySelector('#profile_button');
 	
-	var fab_btn  = document.getElementById('fab_btn');
-	var fab_ctn  = document.getElementById('fab_ctn');
+	var fab_btn  = document.querySelectorAll('.fab_btn');
+	var fab_ctn  = document.querySelectorAll('.fab_ctn');
     var VISIBLE_CLASS = 'is-showing-options';
 	var menu = false;
 
@@ -125,19 +125,24 @@
 		}
 	}
 
-	var showOpts = function(e) {
-		var processClick = function (evt) {
-			if (e !== evt) {
-				fab_ctn.classList.remove(VISIBLE_CLASS);
-				fab_ctn.IS_SHOWING = false;
-				document.removeEventListener('click', processClick);
-			}
-		};
-		if (!fab_ctn.IS_SHOWING) {
-			fab_ctn.IS_SHOWING = true;
-			fab_ctn.classList.add(VISIBLE_CLASS);
-			document.addEventListener('click', processClick);
-		}
-	};
-	fab_btn.addEventListener('click', showOpts);
+	(function () {
+	    var VISIBLE_CLASS = 'is-showing-options',
+	        fab_btn  = document.getElementById('fab_btn'),
+	        fab_ctn  = document.getElementById('fab_ctn'),
+	        showOpts = function(e) {
+	          var processClick = function (evt) {
+	            if (e !== evt) {
+	              fab_ctn.classList.remove(VISIBLE_CLASS);
+	              fab_ctn.IS_SHOWING = false;
+	              document.removeEventListener('click', processClick);
+	            }
+	          };
+	          if (!fab_ctn.IS_SHOWING) {
+	            fab_ctn.IS_SHOWING = true;
+	            fab_ctn.classList.add(VISIBLE_CLASS);
+	            document.addEventListener('click', processClick);
+	          }
+	        };
+	    fab_btn.addEventListener('click', showOpts);
+	}.call(this));
 })();
