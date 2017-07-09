784530fc18bf2d48ea78c71ba262359ff683b000
mathcoll <mathieu@internetcollaboratif.info>
2017-07-09 18:20:57 +0200

    Cleaning code and optimize both rendering and assets loading

--- public/js/m/t6app.js
+++ public/js/m/t6app.js
@@ -1929,5 +1929,25 @@ var containers = {
 			menuElement.style.transform = "translateX(0)";
 		}
 	}, false);
-	
+
+
+	/* *********************************** Offline *********************************** */
+	document.addEventListener('DOMContentLoaded', function(event) {
+		//On initial load to check connectivity
+		if (!navigator.onLine) {
+			updateNetworkStatus();
+		}
+		window.addEventListener('online', updateNetworkStatus, false);
+		window.addEventListener('offline', updateNetworkStatus, false);
+	});
+
+	//To update network status
+	function updateNetworkStatus() {
+		if (navigator.onLine) {
+			toast('You are now online...', {timeout:3000, type: 'done'});
+		}
+		else {
+			toast('You are now offline...', {timeout:3000, type: 'warning'});
+		}
+	}
 })();
