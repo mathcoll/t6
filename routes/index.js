17b86a070df39a366c4a79de0a2e8acf14be502b
mathcoll <mathieu@internetcollaboratif.info>
2017-11-04 21:55:56 +0100

    Add Terms to Api routes

--- routes/index.js
+++ routes/index.js
@@ -417,4 +417,51 @@ router.get('/index', function(req, res, next) {
 	res.status(200).send(index);
 });
 
+
+/**
+ * @api {get} /terms PWA Terms & Privacy
+ * @apiName API Get Terms & Privacy
+ * @apiGroup General
+ * @apiVersion 2.0.1
+ * 
+ * @apiUse 200
+ */
+router.get('/terms', function(req, res, next) {
+	var terms = [
+ 	    {
+			title: 'T6 Privacy',
+			description: 'If you require any more information or have any questions about our privacy policy, please feel free to contact us by email at <a href="mailto:contact@internetcollaboratif.info" rel="noopener" target="_blank">Privacy</a>. At <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> we consider the privacy of our visitors to be extremely important. This privacy policy document describes in detail the types of personal information is collected and recorded by <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> and how we use it.',
+	    },
+ 	    {
+			title: 'Log Files',
+			description: 'Like many other Web sites, <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> makes use of log files. These files merely logs visitors to the site - usually a standard procedure for hosting companies and a part of hosting services\'s analytics. The information inside the log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date/time stamp, referring/exit pages, and possibly the number of clicks. This information is used to analyze trends, administer the site, track user\'s movement around the site, and gather demographic information. IP addresses, and other such information are not linked to any information that is personally identifiable.',
+	    },
+ 	    {
+			title: 'Cookies and Web Beacons',
+			description: '<a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> uses cookies to store information about visitors\' preferences, to record user-specific information on which pages the site visitor accesses or visits, and to personalize or customize our web page content based upon visitors\' browser type or other information that the visitor sends via their browser. <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> has no access to or control over these cookies that are used by third-party advertisers.',
+	    },
+ 	    {
+			title: 'Third Party Privacy Policies',
+			description: 'You should consult the respective privacy policies of these third-party ad servers for more detailed information on their practices as well as for instructions about how to opt-out of certain practices. <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a>\'s privacy policy does not apply to, and we cannot control the activities of, such other advertisers or web sites. You may find a comprehensive listing of these privacy policies and their links here: <a href="http://www.privacypolicyonline.com/privacy-policy-links" title="Privacy Policy Links" rel="noopener" target="_blank">Privacy Policy Links</a>. If you wish to disable cookies, you may do so through your individual browser options. More detailed information about cookie management with specific web browsers can be found at the browsers\' respective websites. <a href="http://www.privacypolicyonline.com/what-are-cookies" rel="noopener" target="_blank">What Are Cookies?</a>',
+	    },
+ 	    {
+			title: 'Children\'s Information',
+			description: 'We believe it is important to provide added protection for children online. We encourage parents and guardians to spend time online with their children to observe, participate in and/or monitor and guide their online activity. <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> does not knowingly collect any personally identifiable information from children under the age of 13. If a parent or guardian believes that <a href="https://api.internetcollaboratif.info" rel="noopener" target="_blank">T6</a> has in its database the personally-identifiable information of a child under the age of 13, please contact us immediately (using the contact in the first paragraph) and we will use our best efforts to promptly remove such information from our records.',
+	    },
+ 	    {
+			title: 'Online Privacy Policy Only',
+			description: 'This privacy policy applies only to our online activities and is valid for visitors to our website and regarding information shared and/or collected there. This policy does not apply to any information collected offline or via channels other than this website.',
+	    },
+ 	    {
+			title: 'Consent',
+			description: 'By using our website T6, you hereby consent to our privacy policy and agree to its terms.',
+	    },
+ 	    {
+			title: 'Update',
+			description: 'This Privacy Policy was last updated on: Saturday, November 04th, 2017. Should we update, amend or make any changes to our privacy policy, those changes will be posted here.',
+	    },
+	];
+	res.status(200).send(terms);
+});
+
 module.exports = router;
