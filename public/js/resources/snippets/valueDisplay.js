7f38710ad69b99518af10d3132d78a0b8c8e7ef2
mathcoll <mathieu@internetcollaboratif.info>
2019-02-20 21:37:27 +0100

    refactorisation

---
+++ public/js/resources/snippets/valueDisplay.js
@@ -0,0 +1,13 @@
+var snippet = {
+	id: "valueDisplay.js",
+	name: "Value Display",
+	options: {
+		color: {defaultVvalue: "#FF0000"},
+		legend: {defaultVvalue: "top", availableValues: [true, false, "top", "bottom"]}
+	},
+	getHtml: function(params) {
+		return "<div class='valuedisplay tile card-valuedisplay material-animate margin-top-4 material-animated mdl-shadow--2dp'>"+params.name+"</div>";
+		//sprintf("<div class='valuedisplay tile card-valuedisplay material-animate margin-top-4 material-animated mdl-shadow--2dp'>%s</div>", "");
+	},
+}
+snippetTypes.push(snippet);
