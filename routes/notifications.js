e4956dad5a9d8e216323021bef4c167580fc6d11
mathcoll <mathieu@internetcollaboratif.info>
2019-03-20 21:45:06 +0100

    Strings must use doublequote. (quotes)

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var express = require("express");
 var router = express.Router();
 var UserSerializer = require("../serializers/user");
@@ -23,13 +23,13 @@ var tokens;
  */
 router.get("/debug/:mail", expressJwt({secret: jwtsettings.secret}), function(req, res) {
 	var mail = req.params.mail;
-	if ( req.user.role === 'admin' ) {
-		res.render('emails/'+mail, {
+	if ( req.user.role === "admin" ) {
+		res.render("emails/"+mail, {
 			currentUrl: req.path,
 			user: req.user
 		});
 	} else {
-		res.status(403).send(new ErrorSerializer({'id': 18, 'code': 403, 'message': 'Forbidden, You should be an Admin!'}).serialize());
+		res.status(403).send(new ErrorSerializer({"id": 18, "code": 403, "message": "Forbidden, You should be an Admin!"}).serialize());
 
 	}
 });
@@ -46,12 +46,12 @@ router.get("/debug/:mail", expressJwt({secret: jwtsettings.secret}), function(re
 router.get("/list/unsubscribed", expressJwt({secret: jwtsettings.secret}), function (req, res) {
 	var user_id = req.user.id;
 	if ( req.user && user_id ) {
-		users	= db.getCollection('users');
+		users	= db.getCollection("users");
 		var json = users.findOne( { id: user_id } );
 		//console.log(json.unsubscription);
 		res.status(200).send({unsubscription: json.unsubscription, unsubscription_token: json.unsubscription_token });
 	} else {
-		res.status(403).send(new ErrorSerializer({'id': 18, 'code': 403, 'message': 'Forbidden'}).serialize());
+		res.status(403).send(new ErrorSerializer({"id": 18, "code": 403, "message": "Forbidden"}).serialize());
 	}
 });
 
@@ -68,34 +68,33 @@ router.get("/list/unsubscribed", expressJwt({secret: jwtsettings.secret}), funct
  * @apiUse 404
  */
 router.get("/mail/reminder", expressJwt({secret: jwtsettings.secret}), function (req, res) {
-	if ( req.user.role === 'admin' ) {
-		users	= db.getCollection('users');
-		//var query = {'token': { '$eq': null }};
-		var query = { '$and': [
-					{'subscription_date': { '$lte': moment().subtract(7, 'days') }},
-					{'reminderMail': undefined},
-					{'token': null},
-					{ '$or': [{'unsubscription': undefined}, {'unsubscription.reminder': undefined}, {'unsubscription.reminder': null}] },
+	if ( req.user.role === "admin" ) {
+		users	= db.getCollection("users");
+		var query = { "$and": [
+					{"subscription_date": { "$lte": moment().subtract(7, "days") }},
+					{"reminderMail": undefined},
+					{"token": null},
+					{ "$or": [{"unsubscription": undefined}, {"unsubscription.reminder": undefined}, {"unsubscription.reminder": null}] },
 				]};
 		var json = users.find( query );
 		
 		if ( json.length > 0 ) {
 			/* Send a Reminder Email to each users */
 			json.forEach(function(user) {
-				res.render('emails/reminder', {user: user}, function(err, html) {
-					var to = user.firstName+' '+user.lastName+' <'+user.email+'>';
+				res.render("emails/reminder", {user: user}, function(err, html) {
+					var to = user.firstName+" "+user.lastName+" <"+user.email+">";
 					var mailOptions = {
 						from: from,
 						bcc: bcc,
 						to: to,
 						list: {
 							unsubscribe: {
-								url: baseUrl_https+'/mail/'+user.email+'/unsubscribe/reminder/'+user.unsubscription_token+'/',
-								comment: 'Unsubscribe from this notification'
+								url: baseUrl_https+"/mail/"+user.email+"/unsubscribe/reminder/"+user.unsubscription_token+"/",
+								comment: "Unsubscribe from this notification"
 							},
 						},
-						subject: 't6 Reminder',
-						text: 'Html email client is required',
+						subject: "t6 Reminder",
+						text: "Html email client is required",
 						html: html
 					};
 					console.log("DEBUG", mailOptions);
@@ -108,10 +107,10 @@ router.get("/mail/reminder", expressJwt({secret: jwtsettings.secret}), function 
 						);
 						db.save();
 					}).catch(function(err){
-						var err = new Error('Internal Error');
+						var err = new Error("Internal Error");
 						err.status = 500;
 						res.status(err.status || 500).render(err.status, {
-							title : 'Internal Error'+app.get('env'),
+							title : "Internal Error"+app.get("env"),
 							user: req.session.user,
 							currentUrl: req.path,
 							err: err
@@ -121,10 +120,10 @@ router.get("/mail/reminder", expressJwt({secret: jwtsettings.secret}), function 
 			});
 			res.status(202).send(new UserSerializer(json).serialize());
 		} else {
-			res.status(404).send(new ErrorSerializer({'id': 20, 'code': 404, 'message': 'Not Found'}).serialize());
+			res.status(404).send(new ErrorSerializer({"id": 20, "code": 404, "message": "Not Found"}).serialize());
 		}
 	} else {
-		res.status(403).send(new ErrorSerializer({'id': 19, 'code': 403, 'message': 'Forbidden '+req.user.role+'/'+process.env.NODE_ENV}).serialize());
+		res.status(403).send(new ErrorSerializer({"id": 19, "code": 403, "message": "Forbidden "+req.user.role+"/"+process.env.NODE_ENV}).serialize());
 	}
 });
 
@@ -141,37 +140,37 @@ router.get("/mail/reminder", expressJwt({secret: jwtsettings.secret}), function 
  * @apiUse 404
  */
 router.get("/mail/changePassword", expressJwt({secret: jwtsettings.secret}), function (req, res) {
-	if ( req.user.role === 'admin' ) {
-		users	= db.getCollection('users');
-		//var query = {'token': { '$eq': null }};
-		var query = { '$and': [
-					{'$or': [{'passwordLastUpdated': { '$lte': moment().subtract(3, 'months') }}, {passwordLastUpdated: undefined}]},
-					{'changePasswordMail': { '$lte': moment().subtract(3, 'months') }},
-					{'token': null},
-					{ '$or': [{'unsubscription': undefined}, {'unsubscription.changePassword': undefined}, {'unsubscription.changePassword': null}] },
+	if ( req.user.role === "admin" ) {
+		users	= db.getCollection("users");
+		//var query = {"token": { "$eq": null }};
+		var query = { "$and": [
+					{"$or": [{"passwordLastUpdated": { "$lte": moment().subtract(3, "months") }}, {passwordLastUpdated: undefined}]},
+					{"changePasswordMail": { "$lte": moment().subtract(3, "months") }},
+					{"token": null},
+					{ "$or": [{"unsubscription": undefined}, {"unsubscription.changePassword": undefined}, {"unsubscription.changePassword": null}] },
 	 			]};
 		var json = users.find( query );
 		if ( json.length > 0 ) {
 			/* Send a Reminder Email to each users */
 			json.forEach(function(user) {
-				console.log(user.firstName+' '+user.lastName+' <'+user.email+'>');
-				console.log(' --> subscription_date:' + user.subscription_date + ' = ' + moment(user.subscription_date, "x").format('DD/MM/YYYY, HH:mm'));
-				console.log(' --> changePasswordMail:' + user.changePasswordMail + ' = ' + moment(user.changePasswordMail, "x").format('DD/MM/YYYY, HH:mm'));
-				console.log(' --> passwordLastUpdated:' + user.passwordLastUpdated + ' = ' + moment(user.passwordLastUpdated, "x").format('DD/MM/YYYY, HH:mm'));
-				res.render('emails/change-password', {user: user}, function(err, html) {
-					var to = user.firstName+' '+user.lastName+' <'+user.email+'>';
+				console.log(user.firstName+" "+user.lastName+" <"+user.email+">");
+				console.log(" --> subscription_date:" + user.subscription_date + " = " + moment(user.subscription_date, "x").format("DD/MM/YYYY, HH:mm"));
+				console.log(" --> changePasswordMail:" + user.changePasswordMail + " = " + moment(user.changePasswordMail, "x").format("DD/MM/YYYY, HH:mm"));
+				console.log(" --> passwordLastUpdated:" + user.passwordLastUpdated + " = " + moment(user.passwordLastUpdated, "x").format("DD/MM/YYYY, HH:mm"));
+				res.render("emails/change-password", {user: user}, function(err, html) {
+					var to = user.firstName+" "+user.lastName+" <"+user.email+">";
 					var mailOptions = {
 						from: from,
 						bcc: bcc,
 						to: to,
 						list: {
 							unsubscribe: {
-								url: baseUrl_https+'/mail/'+user.email+'/unsubscribe/changePassword/'+user.unsubscription_token+'/',
-								comment: 'Unsubscribe from this notification'
+								url: baseUrl_https+"/mail/"+user.email+"/unsubscribe/changePassword/"+user.unsubscription_token+"/",
+								comment: "Unsubscribe from this notification"
 							},
 						},
-						subject: 't6 Password Expiration',
-						text: 'Html email client is required',
+						subject: "t6 Password Expiration",
+						text: "Html email client is required",
 						html: html
 					};
 					console.log("DEBUG", mailOptions);
@@ -184,10 +183,10 @@ router.get("/mail/changePassword", expressJwt({secret: jwtsettings.secret}), fun
 						);
 						db.save();
 					}).catch(function(err){
-						var err = new Error('Internal Error');
+						var err = new Error("Internal Error");
 						err.status = 500;
 						res.status(err.status || 500).render(err.status, {
-							title : 'Internal Error'+app.get('env'),
+							title : "Internal Error"+app.get("env"),
 							user: req.session.user,
 							currentUrl: req.path,
 							err: err
@@ -217,33 +216,32 @@ router.get("/mail/changePassword", expressJwt({secret: jwtsettings.secret}), fun
  * @apiUse 404
  */
 router.get("/mail/newsletter", expressJwt({secret: jwtsettings.secret}), function (req, res) {
-	if ( req.user.role === 'admin' ) {
+	if ( req.user.role === "admin" ) {
 		var year = req.query.year;
 		var template = req.query.template;
-		users	= db.getCollection('users');
-		//var query = {'token': { '$eq': null }};
-		var query = { '$and': [
-					{ '$or': [{'unsubscription': undefined}, {'unsubscription.newsletter': undefined}, {'unsubscription.newsletter': null}] },
+		users	= db.getCollection("users");
+		var query = { "$and": [
+					{ "$or": [{"unsubscription": undefined}, {"unsubscription.newsletter": undefined}, {"unsubscription.newsletter": null}] },
 				]};
 		var json = users.find( query );
 		if ( json.length > 0 && year && template ) {
 			/* Send a newsletter to each subscribers */
 			json.forEach(function(user) {
-				console.log(user.firstName+' '+user.lastName+' <'+user.email+'>');
-				res.render('emails/newsletters/'+year+"/"+template, {user: user}, function(err, html) {
-					var to = user.firstName+' '+user.lastName+' <'+user.email+'>';
+				console.log(user.firstName+" "+user.lastName+" <"+user.email+">");
+				res.render("emails/newsletters/"+year+"/"+template, {user: user}, function(err, html) {
+					var to = user.firstName+" "+user.lastName+" <"+user.email+">";
 					var mailOptions = {
 						from: from,
 						bcc: bcc,
 						to: to,
 						list: {
 							unsubscribe: {
-								url: baseUrl_https+'/mail/'+user.email+'/unsubscribe/newsletter/'+user.unsubscription_token+'/',
-								comment: 'Unsubscribe from this newsletter'
+								url: baseUrl_https+"/mail/"+user.email+"/unsubscribe/newsletter/"+user.unsubscription_token+"/",
+								comment: "Unsubscribe from this newsletter"
 							},
 						},
-						subject: '📰  t6 updates',
-						text: 'Html email client is required',
+						subject: "📰  t6 updates",
+						text: "Html email client is required",
 						html: html
 					};
 					console.log("DEBUG", mailOptions);
@@ -251,15 +249,15 @@ router.get("/mail/newsletter", expressJwt({secret: jwtsettings.secret}), functio
 						users.findAndUpdate(
 								function(i){return i.id==user.id;},
 								function(item){
-									item.newsletter = parseInt(moment().format('x'), 10);
+									item.newsletter = parseInt(moment().format("x"), 10);
 								}
 						);
 						db.save();
 					}).catch(function(err){
-						var err = new Error('Internal Error');
+						var err = new Error("Internal Error");
 						err.status = 500;
 						res.status(err.status || 500).render(err.status, {
-							title : 'Internal Error'+app.get('env'),
+							title : "Internal Error"+app.get("env"),
 							user: req.session.user,
 							currentUrl: req.path,
 							err: err
@@ -269,10 +267,10 @@ router.get("/mail/newsletter", expressJwt({secret: jwtsettings.secret}), functio
 			});
 			res.status(202).send(new UserSerializer(json).serialize());
 		} else {
-			res.status(404).send(new ErrorSerializer({'id': 21, 'code': 404, 'message': 'Not Found'}).serialize());
+			res.status(404).send(new ErrorSerializer({"id": 21, "code": 404, "message": "Not Found"}).serialize());
 		}
 	} else {
-		res.status(403).send(new ErrorSerializer({'id': 18, 'code': 403, 'message': 'Forbidden '+req.user.role+'/'+process.env.NODE_ENV}).serialize());
+		res.status(403).send(new ErrorSerializer({"id": 18, "code": 403, "message": "Forbidden "+req.user.role+"/"+process.env.NODE_ENV}).serialize());
 	}
 });
 
@@ -289,15 +287,15 @@ router.get("/mail/newsletter", expressJwt({secret: jwtsettings.secret}), functio
  * @apiUse 404
  */
 router.post("/resetAllUsersTokens", expressJwt({secret: jwtsettings.secret}), function (req, res) {
-	if ( req.user.role === 'admin' ) {
-		users	= db.getCollection('users');
+	if ( req.user.role === "admin" ) {
+		users	= db.getCollection("users");
 		users.chain().find().update(function(user) {
-			user.unsubscription_token = passgen.create(64, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			user.unsubscription_token = passgen.create(64, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 		});
 		db.save();
-		res.status(200).send({'status': 'done'});
+		res.status(200).send({"status": "done"});
 	} else {
-		res.status(403).send(new ErrorSerializer({'id': 18, 'code': 403, 'message': 'Forbidden '+req.user.role+'/'+process.env.NODE_ENV}).serialize());
+		res.status(403).send(new ErrorSerializer({"id": 18, "code": 403, "message": "Forbidden "+req.user.role+"/"+process.env.NODE_ENV}).serialize());
 	}
 });
 
