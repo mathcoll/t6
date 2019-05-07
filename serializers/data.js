566bd53e440077e8c78072a9504597204be215b7
mathcoll <mathieu@internetcollaboratif.info>
2019-05-07 19:47:49 +0200

    Adjust datapoint serializer to send only relevant details on links

--- serializers/data.js
+++ serializers/data.js
@@ -4,14 +4,14 @@ var JSONAPISerializer = require("jsonapi-serializer").Serializer;
 function DataSerializer(data) {
 	this.serialize = function() {
 		return new JSONAPISerializer("data", {
-	    	keyForAttribute: "underscore_case",
+			keyForAttribute: "underscore_case",
 			attributes : [ "id", "flow_id", "time", "timestamp", "value", "publish", "mqtt_topic", "datatype" ],
 			topLevelLinks : {
-				parent	: data.parent>0?sprintf("%s/v%s/flows/%s", baseUrl_https, version, data.flow_id):null,
-				first	: data.first>0?sprintf("%s/v%s/data/%s?limit=%s&page=1&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.prev, data.order):null,
-				prev	: data.prev>0?sprintf("%s/v%s/data/%s?limit=%s&page=%s&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.prev, data.order):null,
-				next	: data.next>0?sprintf("%s/v%s/data/%s?limit=%s&page=%s&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.next, data.order):null,
-				/*last	: data.last>0?sprintf("%s/v%s/data/%s/p5", baseUrl_https, version, data.flow_id, data.flow_id):null,*/
+				parent	: data.parent>0?sprintf("%s/v%s/flows/%s", baseUrl_https, version, data.flow_id):undefined,
+				first	: data.first>0?sprintf("%s/v%s/data/%s?limit=%s&page=1&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.prev, data.order):undefined,
+				prev	: data.prev>0?sprintf("%s/v%s/data/%s?limit=%s&page=%s&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.prev, data.order):undefined,
+				next	: data.next>0?sprintf("%s/v%s/data/%s?limit=%s&page=%s&order=%s", baseUrl_https, version, data.flow_id, data.limit, data.next, data.order):undefined,
+				last	: data.last>0?sprintf("%s/v%s/data/%s/p5", baseUrl_https, version, data.flow_id, data.flow_id):undefined,
 				title	: data.title,
 				ttl		: data.ttl,
 				unit	: data.unit,
