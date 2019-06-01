35c89b2f413035ff6dae0f6bff2c9ab7ff0a085e
mathcoll <mathieu@internetcollaboratif.info>
2019-06-01 22:07:51 +0200

    Make source better

--- sensors/yoctovoc.js
+++ sensors/yoctovoc.js
@@ -8,25 +8,25 @@ var yoctolib	= require("yoctolib");
 var YAPI = yoctolib.YAPI;
 var YVoc = yoctolib.YVoc;
 
-var res =YAPI.RegisterHub("http://192.168.0.22:4444/"); //127.0.0.1
+var res = YAPI.RegisterHub("http://192.168.0.22:4444/"); //127.0.0.1
 var sensor;
-if(process.argv.length < 3 ||  process.argv[2]=="any" ) {
-    sensor  = YVoc.FirstVoc();   
-    if (sensor==null ) {
-        console.log("No module connected (check USB cable)\n");
-        process.exit(1);
-    }
+if (process.argv.length < 3 || process.argv[2] === "any") {
+	sensor = YVoc.FirstVoc();
+	if (sensor === null) {
+		console.log("No module connected (check USB cable)\n");
+		process.exit(1);
+	}
 } else {
-    var target = process.argv[2];
-    sensor  = YVoc.FindVoc(target+".voc");   
+	var target = process.argv[2];
+	sensor = YVoc.FindVoc(target + ".voc");
 }
 
-var value = '';
-if(!sensor.isOnline()) {
-    console.log("Module not connected (check identification and USB cable)\n");
-    process.exit(1);
+var value = "";
+if (!sensor.isOnline()) {
+	console.log("Module not connected (check identification and USB cable)\n");
+	process.exit(1);
 } else {
 	value = sensor.get_currentValue();
 	console.log(value);
-    process.exit(0);
+	process.exit(0);
 }
