5f6936b2553db09d8e4764d068ef2efa548afae1
mathcoll <mathieu@internetcollaboratif.info>
2019-06-14 20:02:27 +0200

    Add event type to events Db

--- t6decisionrules.js
+++ t6decisionrules.js
@@ -83,7 +83,7 @@ t6decisionrules.checkRulesFromUser = function(user_id, payload) {
 		}
 
 		//console.log("t6App", JSON.stringify({rule_id: event.params.rule_id}), user_id);
-		t6events.add("t6App", JSON.stringify({rule_id: event.params.rule_id}), user_id);
+		t6events.add("t6App", JSON.stringify({rule_id: event.params.rule_id, event_type: event.type}), user_id);
 		if( event.type === "mqttPublish" ) {
 			console.log("Matching EventType", "mqttPublish");
 			let mqttPayload = {dtepoch:payload.dtepoch, value:payload.value, flow: payload.flow};
