04675d7cce3e314a49d91c7a8b72b9b84d9db311
mathcoll <mathieu@internetcollaboratif.info>
2020-05-30 09:52:50 +0200

    Adding X-Frame-Options headers

--- app.js
+++ app.js
@@ -255,6 +255,8 @@ var CrossDomain = function(req, res, next) {
 		res.header("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
 		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Content-Length, X-Requested-With");
 		res.header("Feature-Policy", "accelerometer: 'none'; unsized-media: 'none'; ambient-light-sensor: 'self'; camera: 'none'; encrypted-media: 'none'; fullscreen: 'self'; geolocation: 'self'; gyroscope: 'none'; magnetometer: 'none'; picture-in-picture: 'self'; microphone: 'none'; sync-xhr: 'self'; usb: 'none'; vr: 'none'");
+		res.header("Referrer-Policy", "strict-origin-when-cross-origin");
+		res.header("X-Frame-Options", "SAMEORIGIN");
 		if (req.url.match(/^\/(css|js|img|font)\/.+/)) {
 			res.setHeader("Cache-Control", "public, max-age=3600");
 		}
