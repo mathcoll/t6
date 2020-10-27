986251c5f983b8c5f2c6c208c0a056d59cacf47b
mathcoll <mathieu@internetcollaboratif.info>
2020-10-27 15:17:23 +0100

    include exploration on documentation

--- bin/build-apidoc.sh
+++ bin/build-apidoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apidoc -i ../routes/ -o ../docs/docs/ -f data.js -f datatypes.js -f ota.js -f mqtts.js -f rules.js -f dashboards.js -f snippets.js -f flows.js -f objects.js -f units.js -f users.js -f notifications.js -f index.js --template ../docs/template/
+apidoc -i ../routes/ -o ../docs/docs/ -f exploration.js -f data.js -f datatypes.js -f ota.js -f mqtts.js -f rules.js -f dashboards.js -f snippets.js -f flows.js -f objects.js -f units.js -f users.js -f notifications.js -f index.js --template ../docs/template/
