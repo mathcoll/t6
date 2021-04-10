ba9d29047219e6ce1dbb9400b1559062701d07e6
mathcoll <mathieu@internetcollaboratif.info>
2021-04-10 12:53:05 +0200

    up

--- bin/list_users.sh
+++ bin/list_users.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-jq .collections[0].data[].email < ../data/db-pink.json
+
+# xclip -selection clipboard -i < db-pink.json
+#!/bin/bash
+
+# xclip -selection clipboard -i < db-pink.json
+jq '.collections[0].data[].email' < ../data/db-pink.json
+
+
+jq '.collections[0].data[] | {email: .email, unsubscription: .unsubscription}' < ../data/db-pink.json
+
