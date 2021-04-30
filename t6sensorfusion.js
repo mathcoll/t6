e14b421c18e0694605896c5308b81f3d5a9fab76
mathcoll <mathieu@internetcollaboratif.info>
2021-04-30 22:47:32 +0200

    Add aes encryption to preprocessor

--- t6sensorfusion.js
+++ t6sensorfusion.js
@@ -5,6 +5,21 @@ t6sensorfusion.export = function() {
 	console.dir(JSON.stringify());
 };
 
+
+t6sensorfusion.cryptValue = function(value, sender, encoding) {
+	if ( sender && sender.secret_key_crypt ) {
+		let iv = crypto.randomBytes(16);
+		// sender.secret_key_crypt must be 256 bytes (32 characters)
+		let cipher = crypto.createCipheriv(algorithm, Buffer.from(sender.secret_key_crypt, "hex"), iv);
+		let encrypted = cipher.update(value);
+		encrypted = Buffer.concat([encrypted, cipher.final()]);
+		return iv.toString("hex") + ":" + encrypted.toString("hex");
+	} else {
+		t6console.debug("payload", "Error: Missing secret_key_crypt");
+		return "Error: Missing secret_key_crypt";
+	}
+}
+
 t6sensorfusion.preprocessor = function(flow, payload) {
 	let preprocessor = typeof payload.preprocessor!=="undefined"?payload.preprocessor:(typeof flow.preprocessor!=="undefined"?flow.preprocessor:{});
 	payload.preprocessor = typeof payload.preprocessor!="undefined"?payload.preprocessor:{};
@@ -12,6 +27,21 @@ t6sensorfusion.preprocessor = function(flow, payload) {
 	//if(typeof flow!=="undefined" && typeof flow.preprocessor!=="undefined") {
 		// payload.preprocessor can be an Array
 		switch(preprocessor.type) {
+			case "aes-256-cbc": // aes-256-cbc encryption
+				if(typeof payload.object_id!=="undefined") {
+					let objects	= db.getCollection("objects");
+					let object = objects.findOne({ "$and": [ { "user_id": { "$eq": payload.user_id } }, { "id": { "$eq": payload.object_id } }, ]});
+					if ( object && typeof object.secret_key_crypt!=="undefined" && object.secret_key_crypt.length>0 ) { // TODO: Should also get the Flow.requireCrypted flag.
+						payload.value = t6sensorfusion.cryptValue(""+payload.value, {secret_key_crypt: object.secret_key_crypt});
+						payload.preprocessor.message = `Encrypted using ${payload.object_id}`;
+					} else {
+						payload.preprocessor.message = "Warning: No secret_key found on Object, can't encrypt w/o secret_key from the Object.";
+					}
+				} else {
+					payload.preprocessor.message = "Warning: No Object found on payload, can't encrypt w/o secret_key from the Object.";
+				}
+				break;
+			
 			case "change-case": // Convert strings between camelCase, PascalCase, Capital Case, snake_case and more
 				switch(preprocessor.transform) {
 					case "camelCase":
