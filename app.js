4c262c43a676c193f7aaf2ec1f305b4e5671330e
mathcoll <mathieu@internetcollaboratif.info>
2021-05-30 21:23:48 +0200

    additional database for Data Fusion Buffer

--- app.js
+++ app.js
@@ -226,6 +226,16 @@ var initDbJobs = function() {
 		t6console.log(dbJobs.getCollection("jobs").count(), "resources in Jobs collection.");
 	}
 };
+var initDbFusionBuffer = function() {
+	if ( dbFusionBuffer === null ) {
+		t6console.error("db FusionBuffer is failing");
+	}
+	if ( dbFusionBuffer.getCollection("measures") === null ) {
+		t6console.error("- Collection FusionBuffer is failing");
+	} else {
+		t6console.log(dbFusionBuffer.getCollection("measures").count(), "resources in FusionBuffer collection.");
+	}
+};
 
 t6console.info("Setting correct permission on Databases...");
 let dbs = [
@@ -238,6 +248,7 @@ let dbs = [
 	path.join(__dirname, "data", `otahistory-${os.hostname()}.json`),
 	path.join(__dirname, "data", `uis-${os.hostname()}.json`),
 	path.join(__dirname, "data", `jobs-${os.hostname()}.json`),
+	path.join(__dirname, "data", `fusion-buffer-${os.hostname()}.json`),
 ];
 dbs.forEach(file => {
 	fs.chmod(file, 0o600 , err => {
@@ -258,6 +269,7 @@ dbSources = new loki(path.join(__dirname, "data", `sources-${os.hostname()}.json
 dbOtaHistory = new loki(path.join(__dirname, "data", `otahistory-${os.hostname()}.json`), {autoload: true, autosave: true, autoloadCallback: initDbOtaHistory});
 dbUis = new loki(path.join(__dirname, "data", `uis-${os.hostname()}.json`), {autoload: true, autosave: true, autoloadCallback: initDbUis});
 dbJobs = new loki(path.join(__dirname, "data", `jobs-${os.hostname()}.json`), {autoload: true, autosave: true, autoloadCallback: initDbJobs});
+dbFusionBuffer = new loki(path.join(__dirname, "data", `fusion-buffer-${os.hostname()}.json`), {autoload: true, autosave: true, autoloadCallback: initDbFusionBuffer});
 
 t6console.info("Loading routes...");
 routesLoadTime = new Date();
