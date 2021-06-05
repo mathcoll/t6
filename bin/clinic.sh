f233fc12f14b1774cd9c005355f6468f83ebf15b
mathcoll <mathieu@internetcollaboratif.info>
2021-06-05 08:55:01 +0200

    adding clinic

---
+++ bin/clinic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clinic doctor -- node /home/mathieu/Projets/current/internetcollaboratif.info/t6/bin/www
+#autocannon http://localhost:3000
+
+clinic bubbleprof -- node /home/mathieu/Projets/current/internetcollaboratif.info/t6/bin/www
+#autocannon http://localhost:3000
