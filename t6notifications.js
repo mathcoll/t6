fae144b7597f4725e2ce40d58a6808de9a84b02c
mathcoll <mathieu@internetcollaboratif.info>
2021-07-30 11:50:08 +0200

    bugfix

--- t6notifications.js
+++ t6notifications.js
@@ -30,8 +30,8 @@ if (firebase.admin.serviceAccountFile) {
 					t6events.add("t6App", "sendPush", subscriber_id, subscriber_id, {"endpoint": subscriber.endpoint, "success":  {"statusCode": res.statusCode}});
 					resolve({"status": "info", "info": res});
 				}).catch((err) => {
-					t6events.add("t6App", "sendPush", subscriber_id, subscriber_id, {"endpoint": subscriber.endpoint, "error": {"statusCode": e.statusCode, "body": e.body}});
-					t6console.error(e);
+					t6events.add("t6App", "sendPush", subscriber_id, subscriber_id, {"endpoint": subscriber.endpoint, "error": {"statusCode": err.statusCode, "body": err.body}});
+					t6console.error(err);
 					if(err.statusCode === 404 || err.statusCode === 410) {
 						// User subscription is expired ; we should remove endpoint and keys from Db
 						// find user where "id" == subscriber_id and remove its pushSubscription from lokijs
