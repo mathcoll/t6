e40e73a953421ee167f3c5d84372ef0d7c75be90
mathcoll <mathieu@internetcollaboratif.info>
2021-10-24 21:47:22 +0200

    Add logs to emails

--- t6mailer.js
+++ t6mailer.js
@@ -14,9 +14,11 @@ t6mailer.sendMail = (envelope) => new Promise((resolve, reject) => {
 	if ( process.env.NODE_ENV === "production" ) {
 		transporter.sendMail(envelope).then(function(info) {
 			t6events.addStat("t6App", "sendMail", envelope.user_id, envelope.user_id, {"user_id": envelope.user_id, "status": "info", "info": info});
+			t6events.addAudit("t6App", "sendMail", envelope.user_id, envelope.user_id, {"status": 200});
 			resolve({"status": "info", "info": info});
 		}).catch(function(err) {
 			t6events.addStat("t6App", "sendMail", envelope.user_id, envelope.user_id, {"user_id": envelope.user_id, "status": "error", "info": err});
+			t6events.addAudit("t6App", "sendMail", envelope.user_id, envelope.user_id, {"status": 500, error_id: err});
 			reject({"status": "error", "info": err});
 		});
 	} else {
