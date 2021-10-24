025698b44952bf54645e748184204144fb6b5562
mathcoll <mathieu@internetcollaboratif.info>
2021-10-24 11:20:09 +0200

    Additional automated tests for users, sources pwa, and jobs

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 jsonapitest \
+	$SCRIPT_DIR/../test/pwa.json \
 	$SCRIPT_DIR/../test/t6main.json \
 	$SCRIPT_DIR/../test/objects.json \
 	$SCRIPT_DIR/../test/flows.json \
@@ -13,6 +14,8 @@ jsonapitest \
 	$SCRIPT_DIR/../test/sources.json \
 	$SCRIPT_DIR/../test/exploration.json \
 	$SCRIPT_DIR/../test/admin.json \
+	$SCRIPT_DIR/../test/jobs.json \
+	$SCRIPT_DIR/../test/users.json \
 	$SCRIPT_DIR/../test/cleaning.json
 
 #cat $SCRIPT_DIR/../jsonapitest-results.json | jq '. | length'
