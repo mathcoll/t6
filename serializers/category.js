fdc41a266aec69cd846d1f05587eba5317a99da6
mathcoll <mathieu@internetcollaboratif.info>
2021-11-07 19:21:14 +0100

    add user_id to serializer

--- serializers/category.js
+++ serializers/category.js
@@ -5,7 +5,7 @@ function CategorySerializer(categories) {
 	this.serialize = function() {
 		return new JSONAPISerializer("category", {
 			keyForAttribute: "underscore_case",
-			attributes : [ "name", "color", "description", "meta" ],
+			attributes : [ "name", "color", "description", "user_id", "meta" ],
 			topLevelLinks : {
 				parent : sprintf("%s/v%s/categories", baseUrl_https, version),
 				first : categories.pageFirst!==undefined?sprintf("%s/v%s/flows/?page=%s&size=%s", baseUrl_https, version, categories.pageFirst, categories.size):undefined,
