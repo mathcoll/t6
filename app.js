0834bf66eaa47ecfea0d4e7535ada85f1e1f15eb
mathcoll <mathieu@internetcollaboratif.info>
2021-12-28 21:06:38 +0100

    remove topparser

--- app.js
+++ app.js
@@ -87,7 +87,6 @@ global.slayer			= require("slayer");
 global.strength			= require("strength");
 global.stringformat		= require("string-format");
 global.SunCalc			= require("suncalc");
-global.topparser		= require("topparser");
 global.twilio			= require("twilio");
 global.util				= require("util");
 global.useragent		= require("useragent");
@@ -554,32 +553,6 @@ if (app.get("env") === "development") {
 		} else {
 			t6console.critical(`Uncatch Error on ${app.get("env")}: ${err.message}`, err.status, err.name, err);
 			t6console.error(`Uncatch Error on ${app.get("env")}: ${err.message}`, err.status, err.name, err.stack, req.body, req);
-			var options={
-            //  pid_limit:10,//limit number of included pids in list (default: unlimited)
-            //  pid_filter:(proc)=>{return proc.user=="root"?proc:null},// filtering the pid list (for example: include only pid with user == root) (default: null)
-              pid_sort:(a,b)=>{return a.cpu-b.cpu},// sorting pid list by cpu usage (default)
-            }
-		    topparser.start();
-		
-		    //then data is available
-		    topparser.on("data",data=>{
-		        t6console.error(JSON.stringify(data,0,2));
-		    })
-		
-		    //if some error happens
-		    topparser.on("error",error=>{
-		        t6console.error(error);
-		    })
-		
-		    //if topparser exit
-		    topparser.on("close",code=>{
-		        t6console.error(code);
-		    })
-		
-		    //kill topparser after 10 seconds, for example
-		    setTimeout(()=>{
-		        topparser.stop();
-		    },5000);
 			res.status(err.status || 500).send({ "code": err.status, "error": err.message });
 		}
 		t6events.addStat("t6App", `Error ${err.status} ${err.name}`, "self", t6BuildVersion);
