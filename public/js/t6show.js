e53a12d033d7be8285596989834940dab1d13a24
mathcoll <mathieu@internetcollaboratif.info>
2022-10-15 21:16:20 +0200

    rebuild App

--- public/js/t6show.js
+++ public/js/t6show.js
@@ -284,7 +284,17 @@ let actionate = () => {
 let materializeLight = (inputJson) => {
 	return ml.parse(inputJson) + ml.createSnack();
 };
-
+let uuid = function() {
+	var uuid = "", i, random;
+	for (i = 0; i < 32; i++) {
+		random = Math.random() * 16 | 0;
+		if (i == 8 || i == 12 || i == 16 || i == 20) {
+			uuid += "-";
+		}
+		uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+	}
+	return uuid;
+};
 let registerServiceWorker = function() {
 	return navigator.serviceWorker.register("/sw.js", { scope: "/" })
 	.then(function(registration) {
