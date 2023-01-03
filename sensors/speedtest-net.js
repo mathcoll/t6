fc3dc3c5c10e27d1cd45a93bfd116cc4b6611102
mathcoll <mathieu@internetcollaboratif.info>
2023-01-03 21:26:16 +0100

    update speedtest-net to 2.2.0

--- sensors/speedtest-net.js
+++ sensors/speedtest-net.js
@@ -1,13 +1,33 @@
 #!/usr/bin/node
-
 "use strict";
 
-if (process.argv[2] === "download") {
-	require("speedtest-net")().on("downloadspeed", (speed) => {
-		console.log((speed * 125).toFixed(2));
-	});
-} else if (process.argv[2] === "upload") {
-	require("speedtest-net")().on("uploadspeed", (speed) => {
-		console.log((speed * 125).toFixed(2));
-	});
+const speedTest = require("speedtest-net");
+(async () => {
+	let st;
+	try {
+		st = await speedTest({acceptLicense: true, acceptGdpr: true});
+	} catch (err) {
+		console.log(err.message);
+	} finally {
+		//console.log(st);
+		if (process.argv[2] === "download") {
+			console.log(speedText(st.download.bandwidth));
+		} else if (process.argv[2] === "upload") {
+			console.log(speedText(st.upload.bandwidth));
+		}
+		process.exit(0);
+	}
+})();
+
+function speedText(speed) {
+	let bits = speed * 8;
+	const places = [0, 1, 2, 3, 3];
+	let unit = 0;
+	while (bits >= 2000 && unit < 4) {
+		unit++;
+		bits /= 1000;
+	}
+	//const units = ['', 'K', 'M', 'G', 'T'];
+	//return `${bits.toFixed(places[unit])} ${units[unit]}bps`;
+	return `${bits.toFixed(places[unit])}`;
 }
