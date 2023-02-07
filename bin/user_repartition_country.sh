8697cfa2dc70b46b725a603d0c7367aa2524e49c
mathcoll <mathieu@internetcollaboratif.info>
2023-02-07 21:33:40 +0100

    count countries and sort by count

--- bin/user_repartition_country.sh
+++ bin/user_repartition_country.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-DIR=`dirname "$0"`
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "COUNTRIES"
-jq .collections[].data[].location.geo.country ${DIR}/../data/t6db-users__pink.json
+jq .collections[].data[].location.geo.country ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
 
+
+echo ""
 echo "---------"
 echo "TIMEZONE"
-jq .collections[].data[].location.geo.timezone ${DIR}/../data/t6db-users__pink.json
+jq .collections[].data[].location.geo.timezone ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
