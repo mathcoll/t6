b67f21b9c47392e47647f42d0574ba45fe20c715
mathcoll <mathieu@internetcollaboratif.info>
2023-02-14 20:53:58 +0100

    fix script

--- bin/user_analysis.sh
+++ bin/user_analysis.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DATABASE="t6db-users__pink.json"
 
 echo ""
 echo "---------"
 echo "ROLES"
-jq -cr '.collections[].data[].role' ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
+jq -cr '.collections[].data[].role' ${SCRIPT_DIR}/../data/${DATABASE} | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
 
 
 echo ""
 echo "---------"
 echo "DEVICES"
-jq -cr '.collections[].data[].device' ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
+jq -cr '.collections[].data[].device' ${SCRIPT_DIR}/../data/${DATABASE} | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
 
 
 echo ""
 echo "---------"
 echo "COUNTRIES"
-jq -cr '.collections[].data[].location.geo.country' ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
+jq -cr '.collections[].data[].location.geo.country' ${SCRIPT_DIR}/../data/${DATABASE} | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
 
 
 echo ""
 echo "---------"
 echo "TIMEZONE"
-jq -cr '.collections[].data[].location.geo.timezone | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
+jq -cr '.collections[].data[].location.geo.timezone | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | awk -F, '{a[$1]++;}END{for (i in a)print i, a[i];}' | sort -k 2 -nr 
 
 
 echo ""
 echo "---------"
 echo "USER IPs"
-#jq -cr '(.collections[0].data[]).geoip.ip | select(.email == "xxxxxxx") | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json
-#jq -cr '(.collections[0].data[]).geoip.ip | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json
-jq -cr '.collections[0].data[] | select(.geoip.ip != null) | .email, .geoip.ip' ${SCRIPT_DIR}/../data/t6db-users__pink.json
+jq -cr '.collections[0].data[] | select(.geoip.ip != null) | .email, .geoip.ip' ${SCRIPT_DIR}/../data/${DATABASE}
 
 
 echo ""
 echo "---------"
 echo "USER LAST LOGON AND OTP"
-jq -cr '.collections[0].data[] | select(.lastLogon != null) | {email, lastLogon}' ${SCRIPT_DIR}/../data/t6db-users__pink.json
-jq -cr '.collections[0].data[] | select(.lastOTP != null) | {email, lastOTP}' ${SCRIPT_DIR}/../data/t6db-users__pink.json
+jq -cr '.collections[0].data[] | select(.lastLogon != null) | {email, lastLogon}' ${SCRIPT_DIR}/../data/${DATABASE}
+jq -cr '.collections[0].data[] | select(.lastOTP != null) | {email, lastOTP}' ${SCRIPT_DIR}/../data/${DATABASE}
 
 
 echo ""
 echo "---------"
 echo "USER SUBSCRIPTION"
-totalUsers="$(jq -cr '.collections[].data[] | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | wc -l)"
+totalUsers="$(jq -cr '.collections[].data[] | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | wc -l)"
 echo "- total Users: ${totalUsers}."
-changePassword="$(jq -cr '.collections[].data[].subscription.changePassword | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | wc -l)"
+changePassword="$(jq -cr '.collections[].data[].subscription.changePassword | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | wc -l)"
 echo "- changePassword: ${changePassword} subscribers."
-newsletter="$(jq -cr '.collections[].data[].subscription.newsletter | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | wc -l)"
+newsletter="$(jq -cr '.collections[].data[].subscription.newsletter | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | wc -l)"
 echo "- newsletter: ${newsletter} subscribers."
-monthlyreport="$(jq -cr '.collections[].data[].subscription.monthlyreport | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | wc -l)"
+monthlyreport="$(jq -cr '.collections[].data[].subscription.monthlyreport | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | wc -l)"
 echo "- monthlyreport: ${monthlyreport} subscribers."
-reminder="$(jq -cr '.collections[].data[].subscription.reminder | select(. != null)' ${SCRIPT_DIR}/../data/t6db-users__pink.json | wc -l)"
+reminder="$(jq -cr '.collections[].data[].subscription.reminder | select(. != null)' ${SCRIPT_DIR}/../data/${DATABASE} | wc -l)"
 echo "- reminder: ${reminder} subscribers."
 
 
-
-
-jq -cr '.collections[0].data[] | select(.email == "m.lory@free.fr") | .password' ${SCRIPT_DIR}/../data/t6db-users__pink.json
+echo ""
+echo "---------"
+echo "SPECIFIC USER"
+jq -cr '.collections[0].data[] | select(.email == "") | .password' ${SCRIPT_DIR}/../data/${DATABASE}
 
 
