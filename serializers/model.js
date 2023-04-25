291d54cfeba0adbfb8ff1dceba405b4e88631d5f
mathcoll <mathieu@internetcollaboratif.info>
2023-04-25 21:19:37 +0200

    fix serializer

--- serializers/model.js
+++ serializers/model.js
@@ -1,25 +1,32 @@
 "use strict";
 var JSONAPISerializer = require("jsonapi-serializer").Serializer;
 
-function ModelSerializer(models) {
+function ModelSerializer(model) {
 	this.serialize = function() {
 		return new JSONAPISerializer("model", {
 			keyForAttribute: "underscore_case",
 			attributes : [ "name", "datasets", "flow_ids", "validation_split", "batch_size", "epochs", "history", "labels", "min", "max", "retention" ],
 			topLevelLinks : {
 				parent : sprintf("%s/v%s/models", baseUrl_https, version),
-				first : categories.pageFirst!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageFirst, model.size):undefined,
-				prev : categories.pagePrev!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pagePrev, model.size):undefined,
-				next : categories.pageNext!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageNext, model.size):undefined,
-				last : categories.pageLast!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageLast, model.size):undefined,
+				first : model.pageFirst!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageFirst, model.size):undefined,
+				prev : model.pagePrev!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pagePrev, model.size):undefined,
+				next : model.pageNext!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageNext, model.size):undefined,
+				last : model.pageLast!==undefined?sprintf("%s/v%s/models/?page=%s&size=%s", baseUrl_https, version, model.pageLast, model.size):undefined,
 			},
 			dataLinks : {
+				self : function(model) {
+					if ( typeof model.id!=="undefined" ) {
+						return sprintf("%s/v%s/models/%s", baseUrl_https, version, model.id);
+					} else {
+						return null;
+					}
+				},
 			},
 			objects: {
 				attributes : [],
 				dataLinks: {}
 			}
-		}).serialize(models);
+		}).serialize(model);
 	};
 }
 
